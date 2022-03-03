65d569c8819ed4c3cca19f3a9f2f342445903033
hsh <hansh011@naver.com>
2022-03-03 08:55:01 +0000

    finish

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hyunwoo's app.js v3</code> and save to reload.
+          Edit <code>Hyunwoo's app.js v3 --Good Luck^^---</code> and save to reload.
         </p>
         <a
           className="App-link"
